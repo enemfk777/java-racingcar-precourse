d30c8c0b6ef30b2f4ab88be50cdf560e12c1af8d
ted.kang <ted.kang@kakaocorp.com>
2021-05-02 21:13:58 +0900

    refactor: SingleDigitDiceStrategy 리팩토링
    무조건 0 반환이 아닌 0 - 9 사이의 값을 반환하게 리팩토링

--- src/main/java/domain/strategy/SingleDigitDiceStrategy.java
+++ src/main/java/domain/strategy/SingleDigitDiceStrategy.java
@@ -1,8 +1,14 @@
 package domain.strategy;
 
+import java.util.Random;
+
 public class SingleDigitDiceStrategy implements DiceStrategy {
+
+  private static final int DICE_LIMIT = 10;
+
+  private Random dice = new Random();
   @Override
   public int roll() {
-    return 0;
+    return dice.nextInt(DICE_LIMIT);
   }
 }
