42b71155f6359ddba25823a7a588cdd3b6e3b60f
ted.kang <ted.kang@kakaocorp.com>
2021-05-02 21:22:42 +0900

    refactor: SingleDigitDiceStrategy 리팩토링
    인스턴스 변수 dice를 final로 변경

--- src/main/java/domain/strategy/SingleDigitDiceStrategy.java
+++ src/main/java/domain/strategy/SingleDigitDiceStrategy.java
@@ -6,7 +6,7 @@ public class SingleDigitDiceStrategy implements DiceStrategy {
 
   private static final int DICE_LIMIT = 10;
 
-  private Random dice = new Random();
+  private final Random dice = new Random();
   @Override
   public int roll() {
     return dice.nextInt(DICE_LIMIT);
