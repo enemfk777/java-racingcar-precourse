f880fd17d6d7147d97063a1588b1d6c1e439468b
ted.kang <ted.kang@kakaocorp.com>
2021-05-02 21:21:57 +0900

    refactor: DiceMoveStrategy 리팩토링
    4 이상의 값일 때 true를 반환하는지 확이하는 테스트코드 추가
    테스트 코드 통과를 위한 리팩토링

--- src/main/java/domain/strategy/DiceMoveStrategy.java
+++ src/main/java/domain/strategy/DiceMoveStrategy.java
@@ -1,12 +1,17 @@
 package domain.strategy;
 
 public class DiceMoveStrategy implements MoveStrategy {
-  public DiceMoveStrategy(DiceStrategy diceStrategy, int threshold) {
 
+  private final DiceStrategy diceStrategy;
+  private final int threshold;
+
+  public DiceMoveStrategy(DiceStrategy diceStrategy, int threshold) {
+    this.diceStrategy = diceStrategy;
+    this.threshold = threshold;
   }
 
   @Override
   public boolean isMovable() {
-    return false;
+    return diceStrategy.roll() >= threshold;
   }
 }
--- src/test/java/domain/strategy/DiceMoveStrategyTest.java
+++ src/test/java/domain/strategy/DiceMoveStrategyTest.java
@@ -10,11 +10,19 @@ class DiceMoveStrategyTest {
 
   private static final int THRESHOLD = 4;
 
-  @DisplayName("4 미만의 supplier 값이 들어오면 false 리턴한다.")
+  @DisplayName("4 미만의 값에는 false 리턴한다.")
   @ValueSource(ints = {0, 1, 2, 3})
   @ParameterizedTest
   void isNotMovable(int given) {
     MoveStrategy dice = new DiceMoveStrategy(() -> given, THRESHOLD);
     assertThat(dice.isMovable()).isFalse();
+  }
+
+  @DisplayName("4 이상의 값에는 true 리턴한다.")
+  @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+  @ParameterizedTest
+  void isMovable(int given) {
+    MoveStrategy dice = new DiceMoveStrategy(() -> given, THRESHOLD);
+    assertThat(dice.isMovable()).isTrue();
   }
 }
