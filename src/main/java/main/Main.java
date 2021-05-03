f181dcc8b7b9b21c5cb7d4830a037941893f2a8c
ted.kang <ted.kang@kakaocorp.com>
2021-05-03 22:30:32 +0900

    feat: Main 클래스 작성
    프로그램을 실행하는 Main 클래스 작성

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import domain.ControlTower;
+import dto.AnnouncementSheet;
+import dto.ParticipationForm;
+import ui.Announcer;
+import ui.Reception;
+
+public class Main {
+
+  private Main() {}
+
+  public static void main(String[] args) {
+    ParticipationForm participationForm = Reception.takeParticipationForm();
+    ControlTower controlTower = new ControlTower(participationForm);
+    AnnouncementSheet announcementSheet = new AnnouncementSheet(controlTower.raceStart());
+    Announcer.announcement(announcementSheet);
+  }
+}
