a0e225318808cd42560c5c67b51a76f1f5f87214
ted.kang <ted.kang@kakaocorp.com>
2021-05-03 22:33:02 +0900

    refactor: SingleDigitDiceStrategy 클래스 리팩토링
    newLine으로 Override 메서드 구분

--- src/main/java/domain/strategy/SingleDigitDiceStrategy.java
+++ src/main/java/domain/strategy/SingleDigitDiceStrategy.java
@@ -7,6 +7,7 @@ public class SingleDigitDiceStrategy implements DiceStrategy {
   private static final int DICE_LIMIT = 10;
 
   private final Random dice = new Random();
+
   @Override
   public int roll() {
     return dice.nextInt(DICE_LIMIT);
